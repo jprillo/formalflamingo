aacd7cfb1b13e33654df0e5e185817d69df74288
Jason Prillo <jasonprillo@gmail.com>
2020-02-14 17:13:38 -0500

    change bio info

--- src/components/bio.js
+++ src/components/bio.js
@@ -50,11 +50,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> a web developer in Melbourne FL.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          Follow him on Twitter
         </a>
       </p>
     </div>
