be275b4ccd4adbe2a7bdd5b25a7e7c8aca7fb31f
Jason Prillo <jasonprillo@gmail.com>
2020-05-16 18:50:25 -0400

    addind for js to contact

---
+++ src/pages/thanks.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Layout from "../components/layout"
+import {Link} from "gatsby"
+import jason from "../img/jason.png"
+import sara from "../img/sara.png"
+import "../style/main.scss"
+import SEO from "../components/seo"
+
+
+
+const AboutPage = () => {    
+    return(
+  
+        <div>
+           <Layout>
+    <p>Thank You Very Much</p>
+ 
+
+
+            </Layout>  
+        </div>
+    )
+}
+export default AboutPage
+
