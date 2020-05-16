7745043221549ee075125c32097527ed1ff4c721
Jason Prillo <jasonprillo@gmail.com>
2020-05-16 18:57:44 -0400

    forms suck

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,10 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import {Link} from "gatsby"
-import jason from "../img/jason.png"
-import sara from "../img/sara.png"
+
 import "../style/main.scss"
-import SEO from "../components/seo"
+
 
 
 
